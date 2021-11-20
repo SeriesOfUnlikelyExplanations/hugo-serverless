231078ca962807c122cda71f583060f429b36fe7
Tom Woodard <woodard.thomas@gmail.com>
2021-11-19 22:24:10 -0800

    minor update

--- siteGeneratorLambda/handler.py
+++ siteGeneratorLambda/handler.py
@@ -26,26 +26,30 @@ def run_command(command):
 
 def lambda_handler(event, context):
   logger.info(event)
-  region = event['region']
-  func = boto3.client('lambda', region_name = region)
-  ssm = boto3.client('ssm', region_name = region)
+
   logger.info("Checking the source directory...")
   run_command('ls -n {}'.format(LOCAL_SOURCE_DIR))
   logger.info("Building Hugo site")
   run_command("hugo/hugo -s {0} -d {1}".format(LOCAL_SOURCE_DIR,LOCAL_BUILD_DIR))
   run_command("ls -l {0}".format(LOCAL_BUILD_DIR))
-
+  
+  region = event['region']
+  ssm = boto3.client('ssm', region_name = region)
   parameter = ssm.get_parameter(Name='/OnwardBlog/datasyncSourceTask', WithDecryption=True)
   print(parameter['Parameter']['Value'])
   
   if parameter['Parameter']['Value'] in event['resources'][0]:
     lambdaFunction = ssm.get_parameter(Name='/OnwardBlog/routingLambda', WithDecryption=True)
     d = {'action': 'deploy'}
-    response = client.invoke(
+    func = boto3.client('lambda', region_name = region)
+    response = func.invoke(
       FunctionName=lambdaFunction['Parameter']['Value'],
       LogType='None',
       Payload=json.dumps(d)
     )
+  else:
+    run_command('rm -r {0}'.format(LOCAL_SOURCE_DIR))
+    
   return {"statusCode": 200, \
     "headers": {"Content-Type": "text/html"}, \
     "body": "Build complete"}
