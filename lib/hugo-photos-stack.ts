74d7b23994d04b5863a47aa8698418900013b3c2
Tom Woodard <woodard.thomas@gmail.com>
2021-12-24 17:48:08 -0700

    minor update

--- lib/hugo-photos-stack.ts
+++ lib/hugo-photos-stack.ts
@@ -1,12 +1,16 @@
 import * as cdk from '@aws-cdk/core';
 import { Bucket, BlockPublicAccess } from '@aws-cdk/aws-s3';
-import { User } from '@aws-cdk/aws-iam';
+import { User, AnyPrincipal, PolicyStatement } from '@aws-cdk/aws-iam';
+
+import * as fs from 'fs';
+import * as toml from 'toml';
+const config = toml.parse(fs.readFileSync('./config.toml', 'utf-8'));
 
 export class HugoPhotosStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props: cdk.StackProps) {
     super(scope, id, props);
     //~ //Create a bucket to upload photos to - either directly from a phone or from an upload from a digital camera
-    photoBucket = new Bucket(this, 'Hugo Photos', {
+    const photoBucket = new Bucket(this, 'Hugo Photos', {
       bucketName: config.deploy.siteName+'-photos',
       versioned: false,
       blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
