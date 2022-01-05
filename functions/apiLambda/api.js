7d38f27a1112431af93b12ca3312b0b3297e86a5
Tom Woodard <woodard.thomas@gmail.com>
2022-01-05 07:29:01 -0800

    minor update

--- functions/apiLambda/api.js
+++ functions/apiLambda/api.js
@@ -27,21 +27,13 @@ module.exports = (api, opts) => {
     var comments = []
     if ('post' in req.query) {
       const ddb = new DynamoDB.DocumentClient({signatureVersion: 'v4', region: req.config.region})
-      console.log(req.query.post)
-      console.log(await ddb.query({
-        KeyConditionExpression: 'postPath = :postPath',
-        ExpressionAttributeValues: {
-            ':postPath': req.query.post
-        },
-        TableName: req.config.commentsTable
-       }).promise());
       const comments = await ddb.query({
         KeyConditionExpression: 'postPath = :postPath',
         ExpressionAttributeValues: {
             ':postPath': req.query.post
         },
         TableName: req.config.commentsTable
-       }).promise().then((r) => r.Items.comments)
+       }).promise().then((r) => r.Items[0].comments)
        console.log(comments);
      }
     return res.status(200).json(comments)
