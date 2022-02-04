b293e0a4a9f96bc59d005441bf2c939f0fa7e377
Tom Woodard <woodard.thomas@gmail.com>
2022-02-04 14:40:47 -0800

    minor update

--- functions/apiLambda/test/plan/requestTestData.js
+++ functions/apiLambda/test/plan/requestTestData.js
@@ -16,7 +16,7 @@ module.exports = Object.freeze({
   },
   weather_with_dates: {
     httpMethod: "GET",
-    path: '/api/plan/weather/47.6062/-122.3493?start_date=2-7-2022&finish_date=2-11-2022',
+    path: '/api/plan/weather/47.6062/-122.3321?start_date=2-7-2022&finish_date=2-11-2022',
     resource: '/{proxy+}',
     queryStringParameters: [],
     headers: {Cookie: 'access_token=access_token_noAdmin; id_token=id_token_noAdmin; refresh_token=refresh_token'},
