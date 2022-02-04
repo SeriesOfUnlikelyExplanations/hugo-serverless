f9827681889419e25811e6d7dcbda80b61af591d
Tom Woodard <woodard.thomas@gmail.com>
2022-02-04 14:24:49 -0800

    minor update

--- hugo-serverless-theme/test/test.js
+++ hugo-serverless-theme/test/test.js
@@ -30,7 +30,7 @@ describe('Testing frontend js', function() {
       .get('/api/userInfo')
       .reply(200, JSON.stringify({googleApiKey: "test"}))
       .get('/api/plan/weather/47.6062/-122.3493?start_date=2-7-2022&finish_date=2-11-2022')
-      .reply(200, JSON.stringify({googleApiKey: "test"}));
+      .reply(200, '');
       
     nock('https://api.mapbox.com')
       .persist()
