7812e4f8cc4e43f847124f154f9f762719e44eae
Tom Woodard <woodard.thomas@gmail.com>
2022-02-07 06:45:42 -0800

    minor update

--- hugo-serverless-theme/static/js/functions.js
+++ hugo-serverless-theme/static/js/functions.js
@@ -31,8 +31,8 @@ async function pageLoad(file_path, dependencies = {}) {
     res.set_comments = await setComments(res.status);
   } catch (e) { console.log(e);}
 
-  res.maps = await load_maps(dependencies.mapboxgl || '');
-  res.plan = await plan(dependencies.Litepicker || '', dependencies.google || undefined);
+  res.maps = await load_maps(dependencies.mapboxgl || undefined);
+  res.plan = await plan(dependencies.Litepicker || undefined, dependencies.google || undefined);
   res.post_comment = false;
   if (document.querySelector('#post_comment')) {
     document.querySelector('#post_comment').addEventListener("click", () => { postComment(file_path) });
@@ -195,18 +195,14 @@ async function plan(Litepicker, google) {
       .then((res) => JSON.parse(res))
       .then((data) => {
         var google_maps_script = document.createElement('script');
-        google_maps_script.setAttribute('src',`https://maps.googleapis.com/maps/api/js?key=${data.googleApiKey}&libraries=places&callback=initAutocomplete`);
+        google_maps_script.setAttribute('src',`https://maps.googleapis.com/maps/api/js?key=${data.googleApiKey}&libraries=places&callback=result.plan.initAutocomplete`);
         document.head.appendChild(google_maps_script);
-        if (data.googleApiKey == 'test') {
-          // test artifact - there is probaby a better way to do this...
-          initAutocomplete()
-        }
       });
   }
   //~ const weather = document.querySelector("#weather");
   //~ weather.appendChild(forecastNode);
 
-  return {setupComplete: true, codeAddress: codeAddress, loadWeather: loadWeather, initAutocomplete:initAutocomplete}
+  return {setupComplete: true, codeAddress: codeAddress, loadWeather: loadWeather, initAutocomplete: initAutocomplete }
 };
 
 //
@@ -221,9 +217,8 @@ async function load_maps(mapboxgl) {
   const response = [...maps].map(async function( mapElement ) {
     var request_url = `${window.location.href}${mapElement.id}.geojson`;
     var data = await makeRequest('GET', request_url).then((res) => JSON.parse(res));
-    console.log(mapElement);
     const map = new mapboxgl.Map({
-      container: mapElement,
+      container: mapElement.id,
       center: [data.features[0].properties.longitude, data.features[0].properties.latitude],
       pitch: 45,
       bearing: 0,
